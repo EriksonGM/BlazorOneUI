486a6a4edf7c855a54181db35ccfbb4b83bd0890
Erikson Melgarejo <eriksonlove@gmail.com>
2019-09-25 16:58:03 +0100

    Save

---
+++ BlazorOneUI/Models/Notification.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorOneUI.Models
+{
+    public class Notification
+    {
+        public string Text { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public NotificationTypeEnum NotificationType { get; set; }
+    }
+
+    public enum NotificationTypeEnum
+    {
+        Success = 1,
+        Info,
+        Warning,
+        Danger
+    }
+}
