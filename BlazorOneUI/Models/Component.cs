160054168ae78f7718a46bede93e02c2ba58172d
Erikson Gomes <eriksonlove@gmail.com>
2019-09-29 07:06:58 +0100

    SAve

---
+++ BlazorOneUI/Models/Component.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorOneUI.Models
+{
+    public class Component
+    {
+        public string Display { get; set; }
+
+        public int ComponentType { get; set; }
+
+        public string Value { get; set; } = "";
+
+        public string Pattern { get; set; }
+
+        public bool IsRequired { get; set; }
+
+    }
+
+    public enum ComponentTypeEnum
+    {
+        Textbox = 1,
+        CheckBox,
+        DropDown,
+    }
+}
